f0d0366bf0f73e0cf6145776a37e98a881ee575e
Liam Ronan <liamronan16@gmail.com>
2023-03-28 10:27:59 +0100

    added distance for loops

--- Practice2/sketch.js
+++ Practice2/sketch.js
@@ -12,24 +12,32 @@ function setup () {
     for(let i = 0; i < data.getRowCount(); i++) {
         myArray.push(data.rows[i].obj)
     }
+
+    //Filtering by Year and Label
     let newArray = myArray.filter(row => row.Year == "2018" && row['Statistic Label'] == "Average Red Line Flow");
 
+    //Map
     newArray = newArray.map(row => ({Value: +row.VALUE}));
-
     console.log(newArray);
 
+    //Reduce to one number
     let newArray2 = newArray.reduce(
         (accum, val) => accum + val.Value, 0
     )/(newArray.length)
-
     console.log(newArray2)
 
 
     let distance = [];
-    for(let x = 0; x < newArray.length; x++) {
-        if(x >= 1) {
-           var varOne = newArray[x].Value - newArray[x-1].Value
+    for(let i = 0; i < newArray.length; i++) {
+        for(let x = (i+1); x < newArray.length; x++) {
+            if(i == x) {
+                break;
+            }
+            else {
+                let j = (+newArray[i].Value) - (+newArray[x].Value)
+                distance.push(j)
+            }
         }
-        console.log(varOne)
     }
+    console.log(distance);
 }
